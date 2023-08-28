ad72abb99e2085cce2291407e2a4bb0ad1dbe528
jzhu0054 <jzhu0054@student.monash.edu>
2023-08-28 20:02:56 +1000

    statistic page v1.4

--- frontend/assets/js/map.js
+++ frontend/assets/js/map.js
@@ -1,32 +1,36 @@
-// Set the map's width, height, and margin
-var width = 800;
-var height = 500;
-var margin = { top: 20, right: 20, bottom: 20, left: 20 };
+const width = 800;
+const height = 600;
+const margin = { top: 20, right: 20, bottom: 20, left: 20 };
 
-// Create an SVG element
-var svg = d3.select("#map")
+const svg = d3.select("#map")
     .append("svg")
     .attr("width", width)
     .attr("height", height);
 
-// Load your GeoJSON data
-var geojsonUrl = 'assets/data/Council.geojson';
+// Load and render the GeoJSON data
+d3.json("assets/data/Council.geojson").then(data => {
+    // Create a projection
+    const projection = d3.geoMercator()
+        .fitSize([width, height], data);
 
-// Create a GeoJSON projection
-var projection = d3.geoMercator();
+    // Create a path generator
+    const pathGenerator = d3.geoPath()
+        .projection(projection);
 
-// Create a path generator
-var pathGenerator = d3.geoPath().projection(projection);
-
-// Load and display the GeoJSON data
-d3.json(geojsonUrl).then(function (data) {
-    // Fit the projection to the GeoJSON data
-    projection.fitSize([width, height], data);
-
-    // Append a path for each GeoJSON feature
-    svg.selectAll("path")
+    // Draw the map features
+    const mapPaths = svg.selectAll("path")
         .data(data.features)
         .enter()
         .append("path")
-        .attr("d", pathGenerator);
+        .attr("d", pathGenerator)
+        .attr("fill", "white");
+
+    mapPaths.on("mouseover", function () {
+        d3.select(this)
+            .attr("fill", "blue"); // Change the fill color on mouseover
+    })
+        .on("mouseout", function () {
+            d3.select(this)
+                .attr("fill", "white"); // Change back to white on mouseout
+        });
 });
