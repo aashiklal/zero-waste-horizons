7af24d4a248dc2533a6814bec12b39ce318bf823
jianhu1 <jianhu1@microsoft.com>
2023-09-17 00:11:40 +1000

    add markers and add info windows

--- frontend/assetsDisposePage/js/disposal.js
+++ frontend/assetsDisposePage/js/disposal.js
@@ -45,19 +45,19 @@
   const landfillData = await fetchLandfillData();
 
   const dataTypeDict = {
-    'landfill':landfillData,
-    'transfer':transferStationData,
-    'metal':metalsData,
-    'ewaste':eWasteData
-  }
+    landfill: landfillData,
+    transfer: transferStationData,
+    metal: metalsData,
+    ewaste: eWasteData,
+  };
   function generatePostcodeOptions(types) {
     const dataList = document.getElementById("postcodes");
     const zipCodeSet = new Set();
     types.forEach((type) => {
-      dataTypeDict[type].map((item) => item.Postcode)
-      .forEach((code) => zipCodeSet.add(code));
+      dataTypeDict[type]
+        .map((item) => item.Postcode)
+        .forEach((code) => zipCodeSet.add(code));
     });
-    console.log(zipCodeSet.values());
     Array.from(zipCodeSet).forEach((code) => {
       const option = document.createElement("option");
       option.value = code;
@@ -85,7 +85,7 @@
         }
       }
     });
-    currentSelectedTypes = checkedKeys
+    currentSelectedTypes = checkedKeys;
     generatePostcodeOptions(checkedKeys);
   }
 
@@ -93,24 +93,60 @@
     let totalLat = 0;
     let totalLng = 0;
     let count = 0;
-    locations.forEach(location => {
+    locations.forEach((location) => {
       totalLat += location.Latitude;
       totalLng += location.Longitude;
       count++;
     });
-    
+
     const centerLat = totalLat / count;
     const centerLng = totalLng / count;
+    map.setZoom(13)
     map.setCenter(new google.maps.LatLng(centerLat, centerLng));
+  }
+
+  function addSingleMarker(locationItem,color,type) {
+    const marker = new google.maps.Marker({
+      map: map,
+      position: { lat: locationItem.Latitude, lng: locationItem.Longitude },
+      title: locationItem.UniqueId,
+      icon: `http://maps.google.com/mapfiles/ms/icons/${color}-dot.png`, // Set color
+    });
+    const infoContent = `
+    <div>
+    
+      <h4>${locationItem.FacilityOrSiteName}</h4>
+      <p>Type:${type}</p>
+      <p>Owner: ${locationItem.Owner}</p>
+      <p>Management Type: ${locationItem.ManagementType}</p>
+      <p>Infrastructure Type: ${locationItem.InfrastructureType}</p>
+      <p>Address: ${locationItem.FacilityStreetAddress}, ${locationItem.FacilitySuburbLocality}, ${locationItem.State}, ${locationItem.Postcode}</p>
+    </div>
+  `;
+
+    // Create info window
+    const infoWindow = new google.maps.InfoWindow({
+      content: infoContent,
+    });
+    marker.addListener("mouseover", () => {
+      infoWindow.open(map, marker);
+    });
+
+    marker.addListener("mouseout", () => {
+      infoWindow.close();
+    });
+    return marker
   }
 
   function addMarkerToMap(selectedTypes, postCode) {
     // Remove the previous marker if it exists
     currentMarkers.forEach((marker) => marker.setMap(null));
     currentMarkers = [];
-    const allItems = []
+    const allItems = [];
     selectedTypes.forEach((type) => {
-      let items = dataTypeDict[type].filter((item) => item.Postcode == postCode)
+      let items = dataTypeDict[type].filter(
+        (item) => item.Postcode == postCode
+      );
       let color = undefined;
       switch (type) {
         case "ewaste":
@@ -128,26 +164,20 @@
       }
 
       items.forEach((locationItem) => {
-        const marker = new google.maps.Marker({
-          map: map,
-          position: { lat: locationItem.Latitude, lng: locationItem.Longitude },
-          title: locationItem.UniqueId,
-          icon: `http://maps.google.com/mapfiles/ms/icons/${color}-dot.png`, // Set color
-        });
+        const marker = addSingleMarker(locationItem,color,type)
         currentMarkers.push(marker);
       });
 
-      allItems.push(...items)
+      allItems.push(...items);
     });
-    if(allItems.length) {
-      setItemsCenter(allItems)
+    if (allItems.length) {
+      setItemsCenter(allItems);
     }
-
   }
 
   let map = initMap();
   let currentMarkers = [];
-  let currentSelectedTypes = ["ewaste","landfill","metal","transfer"];
+  let currentSelectedTypes = ["ewaste", "landfill", "metal", "transfer"];
 
   document.getElementById("search-postcode").addEventListener("click", () => {
     const postCode = document.getElementById("post-code-field").value;
