ec29d411add986339b3c6c201e7b1f7d412f126d
jianhu1 <jianhu1@microsoft.com>
2023-09-16 17:08:57 +1000

    add check box logic

--- frontend/assetsDisposePage/js/disposal.js
+++ frontend/assetsDisposePage/js/disposal.js
@@ -1,118 +1,141 @@
 (async function () {
   // Async function to fetch data from E_waste_Table_1
   async function fetchEWasteData() {
-    const dataResponse = await fetch('https://fullmoon.azurewebsites.net/api/e_waste', { mode: 'cors' });
+    const dataResponse = await fetch("http://localhost:3000/api/e_waste", {
+      mode: "cors",
+    });
     const parsedData = await dataResponse.json();
     return parsedData;
   }
 
   // Async function to fetch data from Transfer_Station_Table_1
   async function fetchTransferStationData() {
-    const dataResponse = await fetch('https://fullmoon.azurewebsites.net/api/transfer_station', { mode: 'cors' });
+    const dataResponse = await fetch(
+      "http://localhost:3000/api/transfer_station",
+      { mode: "cors" }
+    );
     const parsedData = await dataResponse.json();
     return parsedData;
   }
 
   // Async function to fetch data from Metals_Table_1
   async function fetchMetalsData() {
-    const dataResponse = await fetch('https://fullmoon.azurewebsites.net/api/metals', { mode: 'cors' });
+    const dataResponse = await fetch("http://localhost:3000/api/metals", {
+      mode: "cors",
+    });
     const parsedData = await dataResponse.json();
     return parsedData;
   }
 
   // Async function to fetch data from Landfill_Table_1
   async function fetchLandfillData() {
-    const dataResponse = await fetch('https://fullmoon.azurewebsites.net/api/landfill', { mode: 'cors' });
+    const dataResponse = await fetch("http://localhost:3000/api/landfill", {
+      mode: "cors",
+    });
     const parsedData = await dataResponse.json();
     return parsedData;
   }
 
   const eWasteData = await fetchEWasteData();
-  console.log('E_waste_Table_1 data:', eWasteData);
 
   const transferStationData = await fetchTransferStationData();
-  console.log('Transfer_Station_Table_1 data:', transferStationData);
 
   const metalsData = await fetchMetalsData();
-  console.log('Metals_Table_1 data:', metalsData);
 
   const landfillData = await fetchLandfillData();
-  console.log('Landfill_Table_1 data:', landfillData);
-
-
-
-  function initMap(target) {
-    // Your data
-    const data = target ?? {
-      UniqueId: "VIC00002",
-      Latitude: -37.7642345,
-      Longitude: 144.9021359,
-      // ... other fields
-    };
-
-    // Create a map object and specify the DOM element for display.
-    const map = new google.maps.Map(document.getElementById('map'), {
-      center: { lat: data.Latitude, lng: data.Longitude },
-      zoom: 11
-    });
-
-    // Create a marker and set its position.
-    const marker = new google.maps.Marker({
-      map: map,
-      position: { lat: data.Latitude, lng: data.Longitude },
-      title: data.UniqueId
-    });
-  }
-  initMap()
-
-  document.getElementById("search-postcode").addEventListener("click", () => {
-    const postCode = document.getElementById('postcodes').value
-    const item = eWasteData.find((item) => item.Postcode == postCode)
-    initMap(item)
-  })
-
-  function generatePostcodeOptions(data) {
-    const dataList = document.getElementById('postcodes');
-    data.map(item => item.Postcode).forEach(code => {
-      const option = document.createElement('option');
+  function generatePostcodeOptions() {
+    const dataList = document.getElementById("postcodes");
+    const zipCodeSet = new Set();
+    eWasteData
+      .map((item) => item.Postcode)
+      .forEach((code) => zipCodeSet.add(code));
+    landfillData
+      .map((item) => item.Postcode)
+      .forEach((code) => zipCodeSet.add(code));
+    metalsData
+      .map((item) => item.Postcode)
+      .forEach((code) => zipCodeSet.add(code));
+    transferStationData
+      .map((item) => item.Postcode)
+      .forEach((code) => zipCodeSet.add(code));
+    console.log(zipCodeSet.values());
+    Array.from(zipCodeSet).forEach((code) => {
+      const option = document.createElement("option");
       option.value = code;
       dataList.appendChild(option);
     });
   }
-  generatePostcodeOptions(eWasteData)
-})()
 
+  function addMarker(map, items, color) {
+    // Create a new marker
+    currentMarker = new google.maps.Marker({
+      map: map,
+      position: { lat: item.Latitude, lng: item.Longitude },
+      title: item.UniqueId,
+      icon: `http://maps.google.com/mapfiles/ms/icons/${color}-dot.png`, // Set color
+    });
+  }
+
+  function initMap() {
+    // Create a map object and specify the DOM element for display.
+    return new google.maps.Map(document.getElementById("map"), {
+      center: { lat: -37.8100243, lng: 144.9643672 },
+      zoom: 11,
+    });
+  }
+
+  function onCheckBoxChange() {
+    const disposalTypeSelect = document.getElementById("checkbox-form");
+    const checkboxes = disposalTypeSelect.querySelectorAll(".form-check-input"); // 获取所有具有 'form-check-input' 类的输入元素
+    const checkedLabels = [];
+
+    checkboxes.forEach((checkbox) => {
+      if (checkbox.checked) {
+        // 获取与选中复选框关联的标签
+        const label = disposalTypeSelect.querySelector(
+          `label[for="${checkbox.id}"]`
+        );
+        // 将标签的文本内容添加到数组中
+        if (label) {
+          checkedLabels.push(label.textContent.trim());
+        }
+      }
+    });
+
+    console.log(checkedLabels);
+  }
+
+  function addMarkerToMap(selectedTypes, postCode) {
+    // Remove the previous marker if it exists
+    if (currentMarker) {
+      currentMarker.setMap(null);
+    }
+    selectedTypes.forEach((type) => {
+      switch (type) {
+        case "E-waste":
+          eWasteData.filter((item) => item.Postcode === postCode);
+          break;
+      }
+    });
+  }
+
+  let map = initMap();
+  let markers = []
+  let currentSelectedTypes = ["E-Waste"];
+  let currentMarker = null;
+
+  document.getElementById("search-postcode").addEventListener("click", () => {
+    const postCode = document.getElementById("postcodes").value;
+    const item = eWasteData.find((item) => item.Postcode == postCode);
+    addMarker(landfillData[0], "red");
+  });
+
+  Array.from(document.getElementsByClassName("form-check-input")).forEach(
+    (ele) => ele.addEventListener("change", onCheckBoxChange)
+  );
+  generatePostcodeOptions();
+})();
 
 // Assuming you have fetched and stored data for ewaste, landfill, metal, and transfer
 
-const disposalTypeSelect = document.getElementById('disposal-type-select');
-
-disposalTypeSelect.addEventListener('change', function () {
-  const selectedOption = document.querySelector('input[name="options"]:checked').value;
-
-  // Depending on the selected option, you can update your data or perform other actions
-  switch (selectedOption) {
-    case 'ewaste':
-      mapDisplayDisposal(eWasteData);
-
-      break;
-    case 'landfill':
-
-      mapDisplayDisposal(landfillData);
-      break;
-    case 'metal':
-
-      mapDisplayDisposal(metalData);
-      break;
-    case 'transfer':
-      mapDisplayDisposal(transferData);
-      break;
-    default:
-      break;
-  }
-});
-
-function mapDisplayDisposal() {
-
-
-}
+function mapDisplayDisposal() {}
