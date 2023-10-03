4fb614bd8ade7b24e70cc73dfd454e7934ccc2db
jianhu1 <jianhu1@microsoft.com>
2023-10-03 21:13:20 +1100

    add classify

--- dev/assetsDisposePage/js/classify.js
+++ dev/assetsDisposePage/js/classify.js
@@ -1,32 +1,3 @@
-let model;
-
-async function loadModel() {
-    // add loading here
-    model = await tf.loadLayersModel("https://fullmoon.azurewebsites.net/api/model/model.json")
-    // loaded
-    console.log(model,111)
-}
-
-async function predict(imageData) {
-    const tensor = tf.browser.fromPixels(imageData, 3);
-    const resized = tf.image.resizeBilinear(tensor, [150, 150]).toFloat();
-    const normalized = resized.div(tf.scalar(255.0));
-    const batched = normalized.expandDims(0);
-
-    const prediction = model.predict(batched);
-    const isRecyclable = prediction.dataSync()[0] > 0.5;
-
-    tensor.dispose();
-    resized.dispose();
-    normalized.dispose();
-    batched.dispose();
-
-    return isRecyclable ? 'Recyclable' : 'Organic';
-}
-
-// 使用
-loadModel()
-
 async function classifyImage() {
     const resultElement = document.getElementById('result');
     const loadingBarContainer = document.getElementById('loading-bar-container');
@@ -49,26 +20,24 @@ async function classifyImage() {
         return;
     }
     
-    const image = new Image();
-    image.src = URL.createObjectURL(input.files[0]);
-    image.onload = async function() {
-        try {
-            const res = await predict(image); // 注意，我们在这里传递的是图像对象，而不是文件对象
-            console.log(res);
+    const formData = new FormData();
+    formData.append('image', input.files[0]);
 
-            resultElement.classList.remove("dotsAnimation");
-            resultElement.textContent = `Classification Result: ${res}`;
+    const response = await fetch('/api/classify', {
+        method: 'POST',
+        body: formData
+    });
+
+    const result = await response.json();
     
-            // Reset loading bar
-            loadingBarContainer.style.display = "none";
-            loadingBar.style.animation = "none";
-            loadingBar.style.width = "0";
-        } catch (error) {
-            console.error("Prediction error:", error);
-        }
-    
-        // Release memory. Important to avoid memory leaks.
-        URL.revokeObjectURL(image.src);
-    }
-    
+    // Wait for 3 seconds before updating the UI with the prediction result
+    setTimeout(() => {
+        resultElement.classList.remove("dotsAnimation");
+        resultElement.textContent = `Classification Result: ${result.prediction}`;
+
+        // Reset loading bar
+        loadingBarContainer.style.display = "none";
+        loadingBar.style.animation = "none";
+        loadingBar.style.width = "0";
+    }, 3000);
 }
