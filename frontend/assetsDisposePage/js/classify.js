0d0a397c8d65ebb1b53b8ca60b08171e57cfd565
Ashiklal Memanaparambil Asokalal <amem0005@student.monash.edu>
2023-10-15 22:52:08 +1100

    made changes as per feedback

--- frontend/assetsDisposePage/js/classify.js
+++ frontend/assetsDisposePage/js/classify.js
@@ -1,13 +1,43 @@
 async function classifyImage() {
+    // Check session storage to see if the user has already accepted the terms.
+    if (sessionStorage.getItem('acceptedTerms')) {
+        // If the user has already accepted the terms, proceed with classification.
+        await proceedWithClassification();
+        return;
+    }
+
+    // First, open the disclaimer modal.
+    let disclaimerModalElement = document.getElementById('disclaimerModal');
+    let disclaimerModal = new bootstrap.Modal(disclaimerModalElement);
+    disclaimerModal.show();
+
+    let modalBody = disclaimerModalElement.querySelector('.modal-body');
+    let acceptButton = document.getElementById('acceptDisclaimer');
+
+    // Reset the button to disabled state every time the modal is shown.
+    acceptButton.disabled = true;
+
+    modalBody.addEventListener('scroll', function() {
+        if (modalBody.scrollHeight - modalBody.scrollTop === modalBody.clientHeight) {
+            acceptButton.disabled = false;
+        }
+    });
+
+    // Attach an event listener to the Accept & Classify button in the modal.
+    acceptButton.addEventListener('click', async function() {
+        sessionStorage.setItem('acceptedTerms', 'true'); // Store acceptance in session storage
+        disclaimerModal.hide(); // Hide the modal
+        await proceedWithClassification(); // Continue with the classification after the user has accepted
+    });
+}
+
+async function proceedWithClassification() {
     const resultElement = document.getElementById('result');
     const loadingBarContainer = document.getElementById('loading-bar-container');
     const loadingBar = document.getElementById('loading-bar');
     
-    // Start the dots animation for "Classifying..."
-    resultElement.textContent = "Classification Result: Classifying";
-
-    // Make sure animation is running
-    resultElement.classList.add("dotsAnimation");
+    // Set the initial message
+    resultElement.textContent = "Classification Result: Classifying...";
 
     // Start loading bar animation
     loadingBarContainer.style.display = "block"; 
@@ -42,12 +72,12 @@ async function classifyImage() {
         
         // Wait for 3 seconds before updating the UI with the prediction result
         setTimeout(() => {
-            resultElement.classList.remove("dotsAnimation");
-            resultElement.textContent = `Classification Result: ${result.prediction}`;
-
             if (result.prediction.toLowerCase() === "hazardous") {
+                resultElement.innerHTML = 'The image is likely <span class="hazardous-text" style="color:#FF1E00;">hazardous</span>. However, always use personal judgment when interpreting results.';
                 const disposeElement = document.getElementById('hero-section');
                 disposeElement.scrollIntoView({ behavior: 'smooth' });
+            } else if (result.prediction.toLowerCase() === "non-hazardous") {
+                resultElement.innerHTML = 'The image is likely <span class="non-hazardous-text" style="color:#f5f5f5;">non-hazardous</span>. However, always use personal judgment when interpreting results.';
             }            
 
             // Reset loading bar
@@ -58,7 +88,6 @@ async function classifyImage() {
 
     } catch (error) {
         console.error("Error during image classification:", error);
-        resultElement.classList.remove("dotsAnimation");
         resultElement.textContent = `${error.message}`;
 
         // Reset loading bar
